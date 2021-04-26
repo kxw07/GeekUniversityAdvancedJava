f9472736a6a3ce6fe79130db8408674de95a8cbe
kai <kai@simbo.com.tw>
2021-04-26 17:26:13 +0800

    check for loop ByteCode

--- src/main/java/week1/HelloByteCode.java
+++ src/main/java/week1/HelloByteCode.java
@@ -1,7 +1,16 @@
 package week1;
 
 public class HelloByteCode {
+
     public static void main(String[] args) {
         HelloByteCode helloByteCode = new HelloByteCode();
+    }
+
+    public void checkForLoopByteCode() {
+        int[] numberArray = {1, 2, 3};
+        int sum = 0;
+        for (int number : numberArray) {
+            sum += number;
+        }
     }
 }
