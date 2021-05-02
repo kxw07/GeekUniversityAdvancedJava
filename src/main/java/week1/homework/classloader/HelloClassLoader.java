add07c4aaf96e03dbe44ecd4aaf53922c9b48fd5
kai <kai@simbo.com.tw>
2021-05-02 20:35:47 +0800

    Resolve classloader.

---
+++ src/main/java/week1/homework/classloader/HelloClassLoader.java
@@ -0,0 +1,51 @@
+package week1.homework.classloader;
+
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class HelloClassLoader extends ClassLoader {
+    public static void main(String[] args) {
+        try {
+            Object obj = new HelloClassLoader().findClass("Hello").newInstance();
+            Method method = obj.getClass().getMethod("hello");
+            method.invoke(obj);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        try (InputStream inputStream = Files.newInputStream(Paths.get("Hello.xlass"))) {
+            StringBuilder hex = new StringBuilder();
+
+            int value;
+            while ((value = inputStream.read()) != -1) {
+                hex.append(String.format("%02x", 255 - value));
+            }
+
+            return defineClass(name, hex2Byte(hex.toString()), 0, hex2Byte(hex.toString()).length);
+        } catch (Exception e) {
+            throw new ClassNotFoundException(e.getMessage(), e);
+        }
+    }
+
+    public byte[] hex2Byte(String hexString) {
+        byte[] bytes = new byte[hexString.length() / 2];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) Integer.parseInt(hexString.substring(2 * i, 2 * i + 2), 16);
+        }
+        return bytes;
+    }
+}
