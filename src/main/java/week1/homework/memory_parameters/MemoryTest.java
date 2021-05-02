0aad92e7a55ba44578798df392f4a2eac6c344da
kai <kai@simbo.com.tw>
2021-05-02 23:32:44 +0800

    add Xss test

---
+++ src/main/java/week1/homework/memory_parameters/MemoryTest.java
@@ -0,0 +1,36 @@
+package week1.homework.memory_parameters;
+
+public class MemoryTest {
+//    javac week1/homework/memory_parameters/MemoryTest.java
+//    java -Xss160k week1/homework/memory_parameters/MemoryTest
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new TestStack("Thread1"));
+        Thread thread2 = new Thread(new TestStack("Thread2"));
+        Thread thread3 = new Thread(new TestStack("Thread3"));
+        Thread thread4 = new Thread(new TestStack("Thread4"));
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+    }
+
+    static class TestStack implements Runnable {
+        private String name;
+
+        public TestStack(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            callLoop(1);
+        }
+
+        public void callLoop(int count) {
+            System.out.println(name + "_" + count);
+            count++;
+            callLoop(count);
+        }
+    }
+}
