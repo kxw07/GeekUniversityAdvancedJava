79eca8356bd4c24ba8a688f71c88a610496aec26
kai <kai@simbo.com.tw>
2021-05-25 11:51:33 +0800

    add aop practice

--- src/main/java/week5/practice/spring_app/SpringApp.java
+++ src/main/java/week5/practice/spring_app/SpringApp.java
@@ -4,27 +4,45 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import week5.practice.spring_app.iml.ISchool;
 import week5.practice.spring_app.obj.Klass;
-import week5.practice.spring_app.obj.School;
 import week5.practice.spring_app.obj.Student;
 
 public class SpringApp {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application.week5.spring.app.xml");
+        System.out.println("=======================");
+        System.out.println("(Student) applicationContext.getBean(\"student001\")");
         Student student001 = (Student) applicationContext.getBean("student001");
         System.out.println(student001);
 
+        System.out.println("=======================");
+        System.out.println("(Student) applicationContext.getBean(\"student002\")");
         Student student002 = (Student) applicationContext.getBean("student002");
         System.out.println(student002);
 
+        System.out.println("=======================");
+        System.out.println("(Klass) applicationContext.getBean(\"class01\")");
         Klass class01 = (Klass) applicationContext.getBean("class01");
         System.out.println(class01);
         class01.dong();
+        System.out.println("class01 real class: " + class01.getClass());
+        System.out.println("Is class01_class a Klass: " + (class01 instanceof Klass));
 
+        System.out.println("=======================");
+        // Note: use xml to config aop, the around behavior is different from annotation setting.
+        // The config setting's after-returning will in the end of aop.
+        System.out.println("applicationContext.getBean(ISchool.class)");
         ISchool school = applicationContext.getBean(ISchool.class);
         System.out.println(school);
         school.ding();
-        System.out.println("School real class: " + school.getClass());
+        System.out.println("ISchool real class: " + school.getClass());
 
+        System.out.println("=======================");;
+        System.out.println("applicationContext.getBean(Klass.class)");
+        Klass class01_class = applicationContext.getBean(Klass.class);
+        System.out.println(class01_class);
+        class01_class.dong();
+        System.out.println("class01_class real class: " + class01_class.getClass());
+        System.out.println("Is class01_class a Klass: " + (class01_class instanceof Klass));
 
     }
 }
--- src/main/java/week5/practice/spring_app/aop/Aop1.java
+++ src/main/java/week5/practice/spring_app/aop/Aop1.java
@@ -3,12 +3,13 @@ package week5.practice.spring_app.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 
 public class Aop1 {
+
     public void around(ProceedingJoinPoint joinPoint) throws Throwable {
         System.out.println("Aop1 around begin ding...");
 
         joinPoint.proceed();
 
-        System.out.println("Aop1 around end ding...");
+        System.out.println("Aop1 around after ding...");
     }
 
     public void startTransaction() {
