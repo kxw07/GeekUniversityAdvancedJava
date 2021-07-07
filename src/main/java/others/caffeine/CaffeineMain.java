35ff78df345e492ea6f44d8aa12f0dcfaa02402a
kai <kai@simbo.com.tw>
2021-07-07 13:11:21 +0800

    update caffeine practice

--- src/main/java/others/caffeine/CaffeineMain.java
+++ src/main/java/others/caffeine/CaffeineMain.java
@@ -1,13 +1,16 @@
 package others.caffeine;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.LoadingCache;
+import com.github.benmanes.caffeine.cache.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.junit.Assert;
 
 import java.util.concurrent.TimeUnit;
 
 public class CaffeineMain {
+    private static final Logger logger = LogManager.getLogger(CaffeineMain.class);
     private static int counter = 0;
 
     public static void main(String[] args) throws InterruptedException {
@@ -18,19 +21,32 @@ public class CaffeineMain {
     private static void loadingCacheWithRefreshAfterWrite() throws InterruptedException {
         LoadingCache<String, String> cache = Caffeine.newBuilder()
                 .refreshAfterWrite(3, TimeUnit.SECONDS)
+                .removalListener(new RemovalListener<String, String>() {
+                    @Override
+                    public void onRemoval(@Nullable String key, @Nullable String value, @NonNull RemovalCause removalCause) {
+                        logger.info("removalListener... key:" + key + ", value:" + value);
+                    }
+                })
                 .maximumSize(100)
                 .build(key -> loadingCache());
 
         Assert.assertEquals("value1", cache.get("key2"));
 
-        Thread.sleep(4000);
+        System.out.println("Sleep 3s");
+        Thread.sleep(3000);
+        Assert.assertEquals("value1", cache.get("key2"));
         Assert.assertEquals("value1", cache.get("key2"));
 
-        Thread.sleep(4000);
+        System.out.println("Sleep 5s");
+        Thread.sleep(5000);
         Assert.assertEquals("value2", cache.get("key2"));
+
+        Thread.sleep(3000);
     }
 
-    private static String loadingCache() {
+    private static String loadingCache() throws InterruptedException {
+        System.out.println("loadingCache...");
+        Thread.sleep(1000);
         return "value" + ++counter;
     }
 
