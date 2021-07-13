43e705e46d8d4bbc5aef03dd27e916a51b56ed94
kai <kai@simbo.com.tw>
2021-07-13 22:32:36 +0800

    add week11 homework 08_02

---
+++ src/main/java/week11/homework/distributed_counter_08_02/DecreaseTask.java
@@ -0,0 +1,26 @@
+package week11.homework.distributed_counter_08_02;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DecreaseTask implements Runnable {
+    private static final Logger logger = LogManager.getLogger(DecreaseTask.class);
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                long stack = RedisUtil.getConnection().decr("counter".getBytes());
+                if (stack >= 0) {
+                    logger.info("Get Stack successfully: " + stack);
+                } else {
+                    throw new Exception("No stack");
+                }
+                Thread.sleep(200);
+            } catch (Exception e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/week11/homework/distributed_counter_08_02/Main.java
@@ -0,0 +1,20 @@
+package week11.homework.distributed_counter_08_02;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        RedisUtil.getConnection().set("counter".getBytes(), String.valueOf(100).getBytes());
+
+        final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
+
+        scheduledExecutorService.schedule(new DecreaseTask(), 5, TimeUnit.SECONDS);
+        scheduledExecutorService.schedule(new DecreaseTask(), 5, TimeUnit.SECONDS);
+
+        while (true) {
+
+        }
+    }
+}
