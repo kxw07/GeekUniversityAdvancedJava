398c38b4b2b563ed8bcdd16293ec8cff31a92295
kai <kai@simbo.com.tw>
2021-07-27 13:00:21 +0800

    update week9

--- src/main/java/week11/homework/distributed_lock_08_01/self_version/LockException.java
+++
@@ -1,7 +0,0 @@
-package week11.homework.distributed_lock_08_01.self_version;
-
-public class LockException extends Exception{
-    public LockException(String message) {
-        super(message);
-    }
-}
