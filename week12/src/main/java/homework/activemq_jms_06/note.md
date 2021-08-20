40d6867abbd503741edecf0a317956100dc884f3
kai <kai@simbo.com.tw>
2021-08-20 13:47:43 +0800

    redir week12

---
+++ week12/src/main/java/homework/activemq_jms_06/note.md
@@ -0,0 +1,8 @@
+1. ActiveMQ info
+   ```
+   bin/activemq start/stop
+   default client port 61616
+   default ui port 8161
+   ```
+   
+2. Example https://activemq.apache.org/version-5-hello-world
---
+++ week12/src/main/java/homework/activemq_jms_06/queue/Main.java
@@ -0,0 +1,14 @@
+package homework.activemq_jms_06.queue;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
+        scheduledExecutorService.scheduleAtFixedRate(new QueueProducer(), 3, 3, TimeUnit.SECONDS);
+        scheduledExecutorService.scheduleAtFixedRate(new QueueConsumer(), 3, 1, TimeUnit.SECONDS);
+        scheduledExecutorService.scheduleAtFixedRate(new QueueConsumer(), 3, 1, TimeUnit.SECONDS);
+    }
+}
